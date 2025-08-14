42f48f7c478e95d7c31ab8f8b2306884ebec8cbd
kiran-shaz <awsclodcomputing@gmail.com>
2025-08-14 00:16:21 -0400

    update

---
+++ src/main/java/com/example/HelloWorld.java
@@ -0,0 +1,23 @@
+pipeline {
+    agent any
+
+    stages {
+        stage('Checkout') {
+            steps {
+                git 'https://github.com/nazishanwar1/maven-practice.git'
+            }
+        }
+
+        stage('Build') {
+            steps {
+                sh 'mvn clean package'
+            }
+        }
+
+        stage('Run') {
+            steps {
+                sh 'java -jar target/maven-practice-1.0-SNAPSHOT.jar'
+            }
+        }
+    }
+}
